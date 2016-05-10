be5ff75359b5c0a31c953508532ff0b76799ae4e
ahmetenis <eniserd63@gmail.com>
2016-05-11 01:54:58 +0300

    tweetDetails.js working now

--- tweetDetails.js
+++ tweetDetails.js
@@ -11,18 +11,24 @@ function getAccountsWithActivity(tweetId, activity) {
   var url = "https://twitter.com/i/activity/" + activity + "_popup?id="+tweetId
   var deferred = q.defer()
   request(url, function(err, response, html) {
-    var json = JSON.parse(html)
-    console.log('getting popup')
-    getJsonRetweetPopup(json.htmlUsers)(function(err, data) {
-      if (err) {
-        console.log("icerde ",err)
-        deferred.reject(err)
-      } else {
-        deferred.resolve(data)
-      }
-    })
+    try {
+      var json = JSON.parse(html)
+      console.log('getting ' + activity + ' popup')
+      getJsonFromPopup(json.htmlUsers)(function(err, data) {
+        if (err) {
+          console.log(err)
+          deferred.reject(err)
+        } else {
+          deferred.resolve(data)
+        }
+      })
+    } catch(e) {
+      console.log("problem", tweetId)
+      console.log(url)
+    }
   })
-  return deferred
+
+  return deferred.promise
 }
 
 function getJsonFromPopup(html) {
@@ -32,7 +38,34 @@ function getJsonFromPopup(html) {
   }])
 }
 
-
 function getActivityDetails(tweetIdList) {
-  
+  tweetIdList.forEach(function(tweetId) {
+    getAccountsWithActivity(tweetId).then(function(response) {
+      var data = {
+        tweetId: tweetId,
+        retweetedAccounts: response 
+      }
+      getAccountsWithActivity(tweetId, 'favorited').then(function(response) {
+        data['favoritedAccounts'] = response
+        fs.appendFileSync("activity.json", JSON.stringify(data) + ",\n")
+      })
+    })
+  })
 }
+
+
+function main() {
+  fs.readFile('syria.json', 'utf-8', function(err, data) {
+    var tweetsJson = JSON.parse("["+data+"]")
+
+    var tweetIdList = tweetsJson[0].map(function(tweet) {
+      return tweet["tweetId"]
+    })
+
+    console.log(tweetIdList)
+
+    getActivityDetails(tweetIdList)
+  })
+}
+fs.writeFileSync("activity.json", "")
+main()
